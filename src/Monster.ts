4f5e192bd70d60c5039de6a63676e948b0908c8c
GugaSantos <guga.santos.guga@gmail.com>
2022-08-09 19:26:52 -0300

    Feat: Classe Dragon criada

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,7 @@
 import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
-  private _lifePoints: number;
+  protected _lifePoints: number;
   private _strength: number;
 
   constructor() {
