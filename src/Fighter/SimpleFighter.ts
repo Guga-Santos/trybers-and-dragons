029668fee0c8459a23f36b5275a88587bbf3f97e
GugaSantos <guga.santos.guga@gmail.com>
2022-08-09 17:10:21 -0300

    Feat: Criada interface SimpleFighter

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,6 @@
+export default interface SimpleFighter {
+  lifePoints: number;
+  strength: number;
+  attack(enemy: SimpleFighter): void;
+  receiveDamage(attackPoints: number): number;
+}
--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,3 +1,5 @@
 import Fighter from './Fighter';
+import SimpleFighter from './SimpleFighter';
 
 export default Fighter;
+export { SimpleFighter };
