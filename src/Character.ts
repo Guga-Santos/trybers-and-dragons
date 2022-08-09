c1aad82d8c64f8ce4666cbb54c73b6f600dd8fcc
GugaSantos <guga.santos.guga@gmail.com>
2022-08-09 17:23:09 -0300

    Feat: Criada a Classe Monster

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -52,7 +52,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
---
+++ src/Monster.ts
@@ -0,0 +1,25 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() { return this._lifePoints; }
+  get strength() { return this._strength; }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) this._lifePoints -= attackPoints;
+    if (this.lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
